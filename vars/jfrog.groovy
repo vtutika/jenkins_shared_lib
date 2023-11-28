8e23a1a03dd4bafe62f8b56c9d8b68520662295b
vtutika <86122101+vtutika@users.noreply.github.com>
2023-11-28 17:59:45 -0500

    Update jfrog.groovy

--- vars/jfrog.groovy
+++ vars/jfrog.groovy
@@ -1,6 +1,5 @@
-def call(){
-    
-    sh """
-    
-    """
+#!/usr/bin/env groovy
+
+def call(String artifactoryUrl, String repository, String filePath, String accessTokenVariable) {
+    sh "jf rt upload --url ${artifactoryUrl} --access-token \${${accessTokenVariable}} ${filePath} ${repository}/"
 }
