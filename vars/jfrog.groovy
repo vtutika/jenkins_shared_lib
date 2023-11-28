d084e85595400bcdf1ea801f74c7a0f58bfeaab3
vtutika <86122101+vtutika@users.noreply.github.com>
2023-11-28 18:07:25 -0500

    Update jfrog.groovy

--- vars/jfrog.groovy
+++ vars/jfrog.groovy
@@ -1,5 +1,5 @@
 #!/usr/bin/env groovy
 
 def call(String artifactoryUrl, String repository, String filePath, String accessTokenVariable) {
-    sh "jf rt upload --url ${artifactoryUrl} --access-token \${${accessTokenVariable}} ${filePath} ${repository}/"
+    sh "jf rt upload --url http://172.17.0.3:8082/artifactory/ --access-token \${ARTIFACTORY_ACCESS_TOKEN} kubernetes-configmap-reload-0.0.1-SNAPSHOT.jar example-repo-local/"
 }
